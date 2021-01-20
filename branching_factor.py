51a84ebc21a12db97c0201be13b9005ff50036a8
Saran-nns <saran_nns@hotmail.com>
2021-01-20 17:29:52 +0100

    Update branching_factor.py

--- branching_factor.py
+++ branching_factor.py
@@ -1,13 +1,17 @@
-def branching_process(N, poisson_rate, t):
-    """Branching_process display a phase transition from asymptotic extinction with probability 1 
-    to asymptotic survival with a positive probability, depending on the average number of created particles.
-    At the critical point, the avalanche size of a branching process is power-law distributed.
-    
-    https://www.nature.com/articles/s41598-020-69705-5
+class BranchingProcess:
+    def __init__(self) -> None:
+        pass
 
-    Args:
-        poisson_rate ([type]): [description]
-        t ([type]): [description]
-    """
+    def branching_process(N, poisson_rate, t):
+        """Branching_process display a phase transition from asymptotic extinction with probability 1 
+        to asymptotic survival with a positive probability, depending on the average number of created particles.
+        At the critical point, the avalanche size of a branching process is power-law distributed.
+        
+        https://www.nature.com/articles/s41598-020-69705-5
 
-    pass
+        Args:
+            poisson_rate ([type]): [description]
+            t ([type]): [description]
+        """
+
+        return NotImplementedError
